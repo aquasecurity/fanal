b393ced468a1bd232f44ebb5dd677b9a34c4f194
Tomoya AMACHI <tomoya.amachi@gmail.com>
2019-05-16 02:31:56 +0900

    add yarn.lock parser

---
+++ analyzer/library/yarn/yarn.go
@@ -0,0 +1,44 @@
+package yarn
+
+import (
+	"bytes"
+	"path/filepath"
+
+	"github.com/knqyf263/fanal/analyzer"
+	"github.com/knqyf263/fanal/extractor"
+	"github.com/knqyf263/fanal/utils"
+	"github.com/knqyf263/go-dep-parser/pkg/types"
+	"github.com/knqyf263/go-dep-parser/pkg/yarn"
+	"golang.org/x/xerrors"
+)
+
+func init() {
+	analyzer.RegisterLibraryAnalyzer(&yarnLibraryAnalyzer{})
+}
+
+type yarnLibraryAnalyzer struct{}
+
+func (a yarnLibraryAnalyzer) Analyze(fileMap extractor.FileMap) (map[analyzer.FilePath][]types.Library, error) {
+	libMap := map[analyzer.FilePath][]types.Library{}
+	requiredFiles := a.RequiredFiles()
+
+	for filename, content := range fileMap {
+		basename := filepath.Base(filename)
+		if !utils.StringInSlice(basename, requiredFiles) {
+			continue
+		}
+
+		r := bytes.NewBuffer(content)
+		libs, err := yarn.Parse(r)
+		if err != nil {
+			return nil, xerrors.Errorf("invalid yarn.lock format: %w", err)
+		}
+		libMap[analyzer.FilePath(filename)] = libs
+	}
+
+	return libMap, nil
+}
+
+func (a yarnLibraryAnalyzer) RequiredFiles() []string {
+	return []string{"yarn.lock"}
+}
