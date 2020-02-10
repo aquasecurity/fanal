c430ac543bc12afea587f36d74ae262a11182844
Simarpreet Singh <simar@linux.com>
2020-02-10 01:20:53 -0800

    docker_test: Add opq and wh file paths.
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- extractor/docker/docker_test.go
+++ extractor/docker/docker_test.go
@@ -353,10 +353,10 @@ func TestExtractor_ExtractLayerFiles(t *testing.T) {
 		name                string
 		fields              fields
 		args                args
-		want                digest.Digest
-		want1               extractor.FileMap
-		want2               []string
-		want3               []string
+		expectedDigest      digest.Digest
+		expectedFileMap     extractor.FileMap
+		expectedOpqDirs     []string
+		expectedWhFiles     []string
 		getLayerExpectation image.GetLayerExpectation
 		wantErr             bool
 	}{
@@ -365,7 +365,7 @@ func TestExtractor_ExtractLayerFiles(t *testing.T) {
 			getLayerExpectation: image.GetLayerExpectation{
 				Args: image.GetLayerArgs{
 					CtxAnything: true,
-					Dig:         "sha256:f75441026d68038ca80e92f342fb8f3c0f1faeec67b5a80c98f033a65beaef5a",
+					Dig:         "sha256:da550bbd659298750df72fc8c1eafe8df272935e1b287bf072f5a630a98a57b4",
 				},
 				Returns: image.GetLayerReturns{
 					Reader: func() io.ReadCloser {
@@ -377,12 +377,43 @@ func TestExtractor_ExtractLayerFiles(t *testing.T) {
 			},
 			args: args{
 				ctx:       nil,
-				dig:       "sha256:f75441026d68038ca80e92f342fb8f3c0f1faeec67b5a80c98f033a65beaef5a",
+				dig:       "sha256:da550bbd659298750df72fc8c1eafe8df272935e1b287bf072f5a630a98a57b4",
 				filenames: []string{"var/foo"},
 			},
-			want: "sha256:f75441026d68038ca80e92f342fb8f3c0f1faeec67b5a80c98f033a65beaef5a",
-			want1: extractor.FileMap{
+			expectedDigest: "sha256:f75441026d68038ca80e92f342fb8f3c0f1faeec67b5a80c98f033a65beaef5a",
+			expectedFileMap: extractor.FileMap{
 				"var/foo": []byte(""),
+			},
+		},
+		{
+			name: "opq file path",
+			getLayerExpectation: image.GetLayerExpectation{
+				Args: image.GetLayerArgs{
+					CtxAnything: true,
+					Dig:         "sha256:852347c2db814b0930034e93053f8bfe9736430431f3b2dbbdf42769592fa626",
+				},
+				Returns: image.GetLayerReturns{
+					Reader: func() io.ReadCloser {
+						f, err := os.Open("testdata/opq.tar")
+						require.NoError(t, err)
+						return f
+					}(),
+				},
+			},
+			args: args{
+				ctx:       nil,
+				dig:       "sha256:852347c2db814b0930034e93053f8bfe9736430431f3b2dbbdf42769592fa626",
+				filenames: []string{"etc/test/"},
+			},
+			expectedDigest: "sha256:852347c2db814b0930034e93053f8bfe9736430431f3b2dbbdf42769592fa626",
+			expectedFileMap: extractor.FileMap{
+				"etc/test/test2": []byte(""),
+			},
+			expectedOpqDirs: []string{
+				"etc/test/",
+			},
+			expectedWhFiles: []string{
+				"var/foo",
 			},
 		},
 	}
@@ -395,22 +426,22 @@ func TestExtractor_ExtractLayerFiles(t *testing.T) {
 				option: tt.fields.option,
 				image:  mockImg,
 			}
-			got, got1, got2, got3, err := d.ExtractLayerFiles(tt.args.ctx, tt.args.dig, tt.args.filenames)
+			actualDigest, actualFileMap, actualOpqDirs, actualWhFiles, err := d.ExtractLayerFiles(tt.args.ctx, tt.args.dig, tt.args.filenames)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ExtractLayerFiles() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if got != tt.want {
-				t.Errorf("ExtractLayerFiles() got = %v, want %v", got, tt.want)
+			if actualDigest != tt.expectedDigest {
+				t.Errorf("ExtractLayerFiles() actualDigest = %v, expectedDigest %v", actualDigest, tt.expectedDigest)
 			}
-			if !reflect.DeepEqual(got1, tt.want1) {
-				t.Errorf("ExtractLayerFiles() got1 = %v, want %v", got1, tt.want1)
+			if !reflect.DeepEqual(actualFileMap, tt.expectedFileMap) {
+				t.Errorf("ExtractLayerFiles() actualFileMap = %v, expectedFileMap %v", actualFileMap, tt.expectedFileMap)
 			}
-			if !reflect.DeepEqual(got2, tt.want2) {
-				t.Errorf("ExtractLayerFiles() got2 = %v, want %v", got2, tt.want2)
+			if !reflect.DeepEqual(actualOpqDirs, tt.expectedOpqDirs) {
+				t.Errorf("ExtractLayerFiles() actualOpqDirs = %v, expectedOpqDirs %v", actualOpqDirs, tt.expectedOpqDirs)
 			}
-			if !reflect.DeepEqual(got3, tt.want3) {
-				t.Errorf("ExtractLayerFiles() got3 = %v, want %v", got3, tt.want3)
+			if !reflect.DeepEqual(actualWhFiles, tt.expectedWhFiles) {
+				t.Errorf("ExtractLayerFiles() actualWhFiles = %v, expectedWhFiles %v", actualWhFiles, tt.expectedWhFiles)
 			}
 		})
 	}
