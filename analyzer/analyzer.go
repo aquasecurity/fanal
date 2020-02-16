dfae47872b8d3afdd0fd9bd3006911f3f6268b8c
knqyf263 <knqyf263@gmail.com>
2020-02-16 13:59:41 +0200

    refactor(analyzer): remove the unused function

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -286,17 +286,3 @@ func GetLibraries(filesMap extractor.FileMap) ([]types.Application, error) {
 	}
 	return results, nil
 }
-
-func mergePkgs(pkgs, pkgsFromCommands []types.Package) []types.Package {
-	uniqPkgs := map[string]struct{}{}
-	for _, pkg := range pkgs {
-		uniqPkgs[pkg.Name] = struct{}{}
-	}
-	for _, pkg := range pkgsFromCommands {
-		if _, ok := uniqPkgs[pkg.Name]; ok {
-			continue
-		}
-		pkgs = append(pkgs, pkg)
-	}
-	return pkgs
-}
