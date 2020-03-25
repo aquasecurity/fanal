1cb88ffbca2d397a40f5ca88ef85d87320a7dad2
knqyf263 <knqyf263@gmail.com>
2020-03-25 11:03:56 +0200

    fix(image): replace containers/image with google/go-containerregistry

--- extractor/image/image_test.go
+++ extractor/image/image_test.go
@@ -3,197 +3,122 @@ package image
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
-	"reflect"
-	"strings"
+	"os"
 	"testing"
 
+	"github.com/aquasecurity/fanal/testutils"
+
 	"github.com/aquasecurity/fanal/types"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 )
 
-func TestNewImage(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		switch {
-		case strings.Contains(r.URL.String(), "unknown"):
-			w.WriteHeader(404)
-			return
-		case strings.Contains(r.URL.String(), "invalid_json"):
-			w.Header().Set("Content-Type", "application/vnd.docker.distribution.manifest.v2+json")
-			w.Write([]byte(`{invalid}`))
-		default:
-			b, _ := ioutil.ReadFile("testdata/manifest.json")
-			w.Header().Set("Content-Type", "application/vnd.docker.distribution.manifest.v2+json")
-			w.Write(b)
-		}
-	}))
-	defer ts.Close()
+func TestNewDockerImage(t *testing.T) {
+	filePaths := map[string]string{
+		"index.docker.io/library/alpine:3.11": "testdata/alpine-310.tar.gz",
+	}
+	te := testutils.NewDockerEngine("1.38", filePaths)
+	defer te.Close()
 
-	tsurl := strings.TrimPrefix(ts.URL, "http://")
+	filePaths = map[string]string{
+		"library/alpine:3.11": "testdata/alpine-310.tar.gz",
+	}
+	tr := testutils.NewDockerRegistry(filePaths)
+	defer tr.Close()
+
+	//pp.Println(ts.Listener.Addr().String())
+	os.Setenv("DOCKER_HOST", fmt.Sprintf("tcp://%s", te.Listener.Addr().String()))
 
 	type args struct {
-		image      Reference
-		transports []string
-		option     types.DockerOption
-	}
-	type image struct {
-		name       string
-		isFile     bool
-		transports []string
+		imageName string
+		option    types.DockerOption
 	}
 	tests := []struct {
-		name      string
-		args      args
-		wantImage image
-		wantErr   string
+		name    string
+		args    args
+		want    v1.Image
+		wantErr bool
 	}{
 		{
-			name: "happy path",
+			name: "happy path with Docker Engine",
 			args: args{
-				image: Reference{
-					Name:   fmt.Sprintf("%s/foobar", tsurl),
-					IsFile: false,
-				},
-				transports: []string{"docker://"},
+				imageName: "alpine:3.11",
+			},
+		},
+		{
+			name: "happy path with Docker Registry",
+			args: args{
+				imageName: fmt.Sprintf("%s/library/alpine:3.11", tr.Listener.Addr().String()),
+			},
+		},
+		{
+			name: "happy path with insecure Docker Registry",
+			args: args{
+				imageName: fmt.Sprintf("%s/library/alpine:3.11", tr.Listener.Addr().String()),
 				option: types.DockerOption{
-					SkipPing:              true,
+					UserName:              "test",
+					Password:              "test",
+					NonSSL:                true,
 					InsecureSkipTLSVerify: true,
 				},
 			},
-			wantImage: image{
-				name:       fmt.Sprintf("%s/foobar", tsurl),
-				isFile:     false,
-				transports: []string{"docker://"},
-			},
 		},
 		{
-			name: "happy path without latest tag",
+			name: "sad path with invalid tag",
 			args: args{
-				image: Reference{
-					Name:   fmt.Sprintf("%s/foobar", tsurl),
-					IsFile: false,
-				},
-				transports: []string{"docker://"},
-				option: types.DockerOption{
-					InsecureSkipTLSVerify: true,
-				},
+				imageName: fmt.Sprintf("%s/library/alpine:3.11!!!", tr.Listener.Addr().String()),
 			},
-			wantImage: image{
-				name:       fmt.Sprintf("%s/foobar", tsurl),
-				isFile:     false,
-				transports: []string{"docker://"},
-			},
+			wantErr: true,
 		},
 		{
-			name: "happy path with a tar file",
+			name: "sad path with non-exist image",
 			args: args{
-				image: Reference{
-					Name:   "testdata/alpine-310.tar.gz",
-					IsFile: true,
-				},
-				transports: []string{"docker-archive:"},
+				imageName: fmt.Sprintf("%s/library/alpine:100", tr.Listener.Addr().String()),
 			},
-			wantImage: image{
-				name:       "testdata/alpine-310.tar.gz",
-				isFile:     true,
-				transports: []string{"docker-archive:"},
-			},
-		},
-		{
-			name: "sad path: invalid image name",
-			args: args{
-				image: Reference{
-					Name:   "ALPINE",
-					IsFile: false,
-				},
-				transports: []string{"docker-archive:"},
-			},
-			wantErr: "invalid image name",
-		},
-		{
-			name: "sad path: invalid image name",
-			args: args{
-				image: Reference{
-					Name:   "alpine:3.10",
-					IsFile: false,
-				},
-				transports: []string{"invalid:"},
-			},
-			wantErr: `unknown transport "invalid"`,
-		},
-		{
-			name: "sad path: non-existed image name",
-			args: args{
-				image: Reference{
-					Name:   fmt.Sprintf("%s/unknown:3.10", tsurl),
-					IsFile: false,
-				},
-				transports: []string{"docker://"},
-				option: types.DockerOption{
-					SkipPing:              true,
-					InsecureSkipTLSVerify: true,
-				},
-			},
-			wantErr: `unexpected end of JSON input`,
-		},
-		{
-			name: "sad path: invalid manifest JSON",
-			args: args{
-				image: Reference{
-					Name:   fmt.Sprintf("%s/invalid_json:3.10", tsurl),
-					IsFile: false,
-				},
-				transports: []string{"docker://"},
-				option: types.DockerOption{
-					SkipPing:              true,
-					InsecureSkipTLSVerify: true,
-				},
-			},
-			wantErr: `failed to initialize: invalid character`,
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ctx := context.Background()
-			got, err := NewImage(ctx, tt.args.image, tt.args.transports, tt.args.option)
-			if tt.wantErr != "" {
-				require.NotNil(t, err, tt.name)
-				require.Contains(t, err.Error(), tt.wantErr, tt.name)
+			fmt.Println(os.Getenv("DOCKER_HOST"))
+			_, err := NewDockerImage(context.Background(), tt.args.imageName, tt.args.option)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewDockerImage() error = %v, wantErr %v", err, tt.wantErr)
 				return
-			} else {
-				require.NoError(t, err, tt.name)
 			}
-
-			assert.Equal(t, tt.wantImage.name, got.name, tt.name)
 		})
 	}
 }
 
-func TestRealImage_LayerIDs(t *testing.T) {
+func TestNewDockerArchiveImage(t *testing.T) {
+	type args struct {
+		fileName string
+	}
 	tests := []struct {
-		name      string
-		imageFile string
-		want      []string
+		name    string
+		args    args
+		want    v1.Image
+		wantErr bool
 	}{
 		{
-			name:      "happy path",
-			imageFile: "testdata/alpine-310.tar.gz",
-			want:      []string{"sha256:531743b7098cb2aaf615641007a129173f63ed86ca32fe7b5a246a1c47286028"},
+			name: "happy path",
+			args: args{
+				fileName: "testdata/alpine-310.tar.gz",
+			},
+		},
+		{
+			name: "sad path",
+			args: args{
+				fileName: "testdata/invalid.tar.gz",
+			},
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			img, err := NewImage(context.Background(), Reference{
-				Name:   tt.imageFile,
-				IsFile: true,
-			}, []string{"docker-archive:"}, types.DockerOption{})
-			require.NoError(t, err, tt.name)
-
-			if got := img.LayerIDs(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("LayerIDs() = %v, want %v", got, tt.want)
+			_, err := NewDockerArchiveImage(tt.args.fileName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewDockerArchiveImage() error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
 		})
 	}
