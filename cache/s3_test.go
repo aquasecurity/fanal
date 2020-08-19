aefe7d43dfa9bae7834f37107f73fd34d29d0feb
Simarpreet Singh <simar@linux.com>
2020-08-19 16:49:28 -0700

    s3: Beef up tests for MissingBlobs
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- cache/s3_test.go
+++ cache/s3_test.go
@@ -1,15 +1,22 @@
 package cache
 
 import (
-	"reflect"
+	"errors"
+	"io"
+	"strings"
 	"testing"
 	"time"
+
+	"golang.org/x/xerrors"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/aws/aws-sdk-go/service/s3/s3manager/s3manageriface"
 
 	"github.com/aquasecurity/fanal/types"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"golang.org/x/xerrors"
 )
 
 type mockS3Client struct {
@@ -169,22 +176,38 @@ func TestS3Cache_getIndex(t *testing.T) {
 
 type mockS3ClientMissingBlobs struct {
 	s3iface.S3API
+	headObjectFunc func(*s3.HeadObjectInput) (*s3.HeadObjectOutput, error)
 }
 
 func (m *mockS3ClientMissingBlobs) PutObject(*s3.PutObjectInput) (*s3.PutObjectOutput, error) {
 	return &s3.PutObjectOutput{}, nil
 }
 
-func (m *mockS3ClientMissingBlobs) HeadObject(*s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
-	return &s3.HeadObjectOutput{}, xerrors.Errorf("the object doesn't exist in S3")
+func (m *mockS3ClientMissingBlobs) HeadObject(hio *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+	if m.headObjectFunc != nil {
+		return m.headObjectFunc(hio)
+	}
+
+	return &s3.HeadObjectOutput{}, nil
+}
+
+type mockS3Downloader struct {
+	s3manageriface.DownloaderAPI
+	download func(io.WriterAt, *s3.GetObjectInput, ...func(*s3manager.Downloader)) (int64, error)
+}
+
+func (m *mockS3Downloader) Download(w io.WriterAt, input *s3.GetObjectInput, downloadFunc ...func(*s3manager.Downloader)) (int64, error) {
+	if m.download != nil {
+		return m.download(w, input, downloadFunc...)
+	}
+
+	return 0, nil
 }
 
 func TestS3Cache_MissingBlobs(t *testing.T) {
-	mockSvc := &mockS3ClientMissingBlobs{}
-
 	type fields struct {
 		S3         s3iface.S3API
-		Downloader *s3manager.Downloader
+		Downloader s3manageriface.DownloaderAPI
 		BucketName string
 		Prefix     string
 	}
@@ -193,39 +216,189 @@ func TestS3Cache_MissingBlobs(t *testing.T) {
 		blobIDs    []string
 	}
 	tests := []struct {
-		name            string
-		fields          fields
-		args            args
-		want            bool
-		wantStringSlice []string
-		wantErr         bool
-	}{{
-		name: "happy path",
-		fields: fields{
-			S3:         mockSvc,
-			BucketName: "test",
-			Prefix:     "prefix",
+		name                 string
+		fields               fields
+		args                 args
+		want                 bool
+		wantMissingBlobSlice []string
+		wantErr              string
+	}{
+		{
+			name: "happy path, with missing blob, with missing artifact",
+			fields: fields{
+				S3: &mockS3ClientMissingBlobs{
+					headObjectFunc: func(input *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+						return &s3.HeadObjectOutput{}, xerrors.Errorf("the object doesn't exist in S3")
+					},
+				},
+				BucketName: "test",
+				Prefix:     "prefix",
+			},
+			args: args{
+				artifactID: "sha256:58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4",
+				blobIDs:    []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+			},
+			want:                 true,
+			wantMissingBlobSlice: []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
 		},
-		args:            args{artifactID: "sha256:58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4", blobIDs: []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"}},
-		want:            true,
-		wantStringSlice: []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
-		wantErr:         false,
-	},
+		{
+			name: "happy path, blob schema mismatch",
+			fields: fields{
+				S3: &mockS3ClientMissingBlobs{
+					headObjectFunc: func(input *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+						if strings.Contains(*input.Key, "24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7") {
+							return &s3.HeadObjectOutput{}, xerrors.Errorf("the object doesn't exist in S3")
+						}
+
+						return &s3.HeadObjectOutput{}, nil
+					},
+				},
+				Downloader: &mockS3Downloader{download: func(at io.WriterAt, input *s3.GetObjectInput, f ...func(*s3manager.Downloader)) (int64, error) {
+					// blob
+					if strings.Contains(*input.Key, "24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7") {
+						at.WriteAt([]byte(`{
+							"Version": 666,
+							"Digest": "sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"
+						}`), 0)
+						return 0, nil
+					}
+
+					// artifact
+					if strings.Contains(*input.Key, "58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4") {
+						at.WriteAt([]byte(`{
+							"Version": 1
+						}`), 0)
+						return 0, nil
+					}
+
+					return 0, nil
+				}},
+				BucketName: "test",
+				Prefix:     "prefix",
+			},
+			args: args{
+				artifactID: "sha256:58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4",
+				blobIDs:    []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+			},
+			want:                 false,
+			wantMissingBlobSlice: []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+		},
+		{
+			name: "happy path, artifact schema mismatch",
+			fields: fields{
+				S3: &mockS3ClientMissingBlobs{
+					headObjectFunc: func(input *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+						if strings.Contains(*input.Key, "24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7") {
+							return &s3.HeadObjectOutput{}, xerrors.Errorf("the object doesn't exist in S3")
+						}
+
+						return &s3.HeadObjectOutput{}, nil
+					},
+				},
+				Downloader: &mockS3Downloader{download: func(at io.WriterAt, input *s3.GetObjectInput, f ...func(*s3manager.Downloader)) (int64, error) {
+					// blob
+					if strings.Contains(*input.Key, "24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7") {
+						at.WriteAt([]byte(`{
+							"Version": 1,
+							"Digest": "sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"
+						}`), 0)
+						return 0, nil
+					}
+
+					// artifact
+					if strings.Contains(*input.Key, "58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4") {
+						at.WriteAt([]byte(`{
+							"Version": 666
+						}`), 0)
+						return 0, nil
+					}
+
+					return 0, nil
+				}},
+				BucketName: "test",
+				Prefix:     "prefix",
+			},
+			args: args{
+				artifactID: "sha256:58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4",
+				blobIDs:    []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+			},
+			want:                 true,
+			wantMissingBlobSlice: []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+		},
+		{
+			name: "sad path, with missing blob, with present artifact",
+			fields: fields{
+				S3: &mockS3ClientMissingBlobs{
+					headObjectFunc: func(input *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+						// error if blob
+						if strings.Contains(*input.Key, "24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7") {
+							return &s3.HeadObjectOutput{}, xerrors.Errorf("the object doesn't exist in S3")
+						}
+
+						// ok if artifact
+						return &s3.HeadObjectOutput{}, nil
+					},
+				},
+				Downloader: &mockS3Downloader{download: func(at io.WriterAt, input *s3.GetObjectInput, f ...func(*s3manager.Downloader)) (int64, error) {
+					return -1, errors.New("download failed")
+				}},
+				BucketName: "test",
+				Prefix:     "prefix",
+			},
+			args: args{
+				artifactID: "sha256:58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4",
+				blobIDs:    []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+			},
+			want:                 true,
+			wantMissingBlobSlice: []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+			wantErr:              "the artifact object (sha256:58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4) doesn't exist in S3 even though the index file exists",
+		},
+		{
+			name: "sad path, blob download failure",
+			fields: fields{
+				S3: &mockS3ClientMissingBlobs{
+					headObjectFunc: func(input *s3.HeadObjectInput) (*s3.HeadObjectOutput, error) {
+						if strings.Contains(*input.Key, "24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7") {
+							return &s3.HeadObjectOutput{}, xerrors.Errorf("the object doesn't exist in S3")
+						}
+
+						return &s3.HeadObjectOutput{}, nil
+					},
+				},
+				Downloader: &mockS3Downloader{download: func(at io.WriterAt, input *s3.GetObjectInput, f ...func(*s3manager.Downloader)) (int64, error) {
+					if strings.Contains(*input.Key, "24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7") {
+						at.WriteAt([]byte(`{
+							"Version": 1,
+							"Digest": "sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"
+						}`), 0)
+						return 0, nil
+					}
+					return -1, errors.New("download failed")
+				}},
+				BucketName: "test",
+				Prefix:     "prefix",
+			},
+			args: args{
+				artifactID: "sha256:58701fd185bda36cab0557bb6438661831267aa4a9e0b54211c4d5317a48aff4",
+				blobIDs:    []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7", "sha256:ffffff4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+			},
+			want:                 true,
+			wantMissingBlobSlice: []string{"sha256:24df0d4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7"},
+			wantErr:              "the blob object (sha256:ffffff4e20c0f42d3703bf1f1db2bdd77346c7956f74f423603d651e8e5ae8a7) doesn't exist in S3 even though the index file exists",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := NewS3Cache(tt.fields.BucketName, tt.fields.Prefix, tt.fields.S3, tt.fields.Downloader)
-			got, got1, err := c.MissingBlobs(tt.args.artifactID, tt.args.blobIDs)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("S3Cache.MissingBlobs() error = %v, wantErr %v", err, tt.wantErr)
-				return
+			gotBool, gotSlice, err := c.MissingBlobs(tt.args.artifactID, tt.args.blobIDs)
+			switch {
+			case tt.wantErr != "":
+				assert.Contains(t, err.Error(), tt.wantErr, tt.name)
+			default:
+				assert.NoError(t, err, tt.name)
 			}
-			if got != tt.want {
-				t.Errorf("S3Cache.MissingBlobs() got = %v, want %v", got, tt.want)
-			}
-			if !reflect.DeepEqual(got1, tt.wantStringSlice) {
-				t.Errorf("S3Cache.MissingBlobs() got1 = %v, want %v", got1, tt.wantStringSlice)
-			}
+			assert.Equal(t, tt.want, gotBool, tt.name)
+			assert.Equal(t, tt.wantMissingBlobSlice, gotSlice, tt.name)
 		})
 	}
 }
