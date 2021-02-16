69a5cd431f882f024f5595a5869ddab2d8d82e38
knqyf263 <knqyf263@gmail.com>
2021-02-16 09:00:20 +0200

    fix: pass disable analyzers to constructors

--- artifact/artifact.go
+++ artifact/artifact.go
@@ -3,14 +3,9 @@ package artifact
 import (
 	"context"
 
-	"github.com/aquasecurity/fanal/analyzer"
 	"github.com/aquasecurity/fanal/types"
 )
 
-type InspectOption struct {
-	DisableAnalyzers []analyzer.Type
-}
-
 type Artifact interface {
-	Inspect(ctx context.Context, option InspectOption) (reference types.ArtifactReference, err error)
+	Inspect(ctx context.Context) (reference types.ArtifactReference, err error)
 }
