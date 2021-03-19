d6b8630898e62792c2638b5b64f4e4e05a258327
ronaudinho <doelaudi@gmail.com>
2021-03-19 13:51:11 +0700

    fix unexpected mock calls in tests

--- cache/key_test.go
+++ cache/key_test.go
@@ -26,15 +26,12 @@ func TestWithVersionSuffix(t *testing.T) {
 				key:     "sha256:5c534be56eca62e756ef2ef51523feda0f19cd7c15bb0c015e3d6e3ae090bf6e",
 				version: "111101112110013",
 			},
-			want: "sha256:18d64117ccf048ed38c21e98c57dbbde66b181edc1bde792256eaa931bf0f7b1",
+			want: "sha256:1acfb4a0eb0acb0b1e8d462df18841581b960d5df190b32f3327df4820e5bc7b",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			opt := &config.ScannerOption{
-				FilePatterns: []string{":"},
-			}
-			got, err := CalcKey(tt.args.key, tt.args.version, opt)
+			got, err := CalcKey(tt.args.key, tt.args.version, &config.ScannerOption{})
 			if tt.wantErr != "" {
 				require.Error(t, err)
 				assert.Contains(t, err.Error(), tt.wantErr)
