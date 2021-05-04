c1dbb928f67e586f45a8fe653d97037a849a79e0
knqyf263 <knqyf263@gmail.com>
2021-05-04 16:12:09 +0900

    feat(import): add gobinary and gomod

--- analyzer/all/import.go
+++ analyzer/all/import.go
@@ -5,6 +5,8 @@ import (
 	_ "github.com/aquasecurity/fanal/analyzer/library/bundler"
 	_ "github.com/aquasecurity/fanal/analyzer/library/cargo"
 	_ "github.com/aquasecurity/fanal/analyzer/library/composer"
+	_ "github.com/aquasecurity/fanal/analyzer/library/gobinary"
+	_ "github.com/aquasecurity/fanal/analyzer/library/gomod"
 	_ "github.com/aquasecurity/fanal/analyzer/library/jar"
 	_ "github.com/aquasecurity/fanal/analyzer/library/npm"
 	_ "github.com/aquasecurity/fanal/analyzer/library/nuget"
