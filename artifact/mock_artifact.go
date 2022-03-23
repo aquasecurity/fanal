bff8cf20c6def2a59850eb80c34d495811ee8a2b
AMF <work@afdesk.com>
2022-03-23 17:57:18 +0600

    Update MockArtifact and change `Clean` to empty method

--- artifact/mock_artifact.go
+++ artifact/mock_artifact.go
@@ -14,6 +14,41 @@ type MockArtifact struct {
 	mock.Mock
 }
 
+type ArtifactCleanArgs struct {
+	_a0         types.ArtifactReference
+	_a0Anything bool
+}
+
+type ArtifactCleanReturns struct {
+	_a0 error
+}
+
+type ArtifactCleanExpectation struct {
+	Args    ArtifactCleanArgs
+	Returns ArtifactCleanReturns
+}
+
+func (_m *MockArtifact) ApplyCleanExpectation(e ArtifactCleanExpectation) {
+	var args []interface{}
+	if e.Args._a0Anything {
+		args = append(args, mock.Anything)
+	} else {
+		args = append(args, e.Args._a0)
+	}
+	_m.On("Clean", args...).Return(e.Returns._a0)
+}
+
+func (_m *MockArtifact) ApplyCleanExpectations(expectations []ArtifactCleanExpectation) {
+	for _, e := range expectations {
+		_m.ApplyCleanExpectation(e)
+	}
+}
+
+// Clean provides a mock function with given fields: _a0
+func (_m *MockArtifact) Clean(_a0 types.ArtifactReference) error {
+	return nil
+}
+
 type ArtifactInspectArgs struct {
 	Ctx         context.Context
 	CtxAnything bool
