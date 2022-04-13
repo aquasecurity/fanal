891ed41e1243298f041dced08362ade2e238b1c3
Lior Vaisman <lior@argon.io>
2022-04-13 16:41:37 +0300

    merge test files

--- analyzer/secret/secret_test.go
+++ analyzer/secret/secret_test.go
@@ -81,3 +81,50 @@ func TestSecretAnalyzer(t *testing.T) {
 		})
 	}
 }
+
+func TestSecretRequire(t *testing.T) {
+	tests := []struct {
+		name     string
+		filePath string
+		want     bool
+	}{
+		{
+			name:     "pass regular file",
+			filePath: "testdata/secret.txt",
+			want:     true,
+		},
+		{
+			name:     "skip small file",
+			filePath: "testdata/emptyfile",
+			want:     false,
+		},
+		{
+			name:     "skip folder",
+			filePath: "testdata/node_modules/secret.txt",
+			want:     false,
+		},
+		{
+			name:     "skip file",
+			filePath: "testdata/package-lock.json",
+			want:     false,
+		},
+		{
+			name:     "skip extension",
+			filePath: "testdata/secret.doc",
+			want:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a, err := secret.NewSecretAnalyzer("")
+			require.NoError(t, err)
+
+			fi, err := os.Stat(tt.filePath)
+			require.NoError(t, err)
+
+			got := a.Required(tt.filePath, fi)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
