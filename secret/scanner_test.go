32d15456f070b729ecc3c2bbb1c2ee5d447a5c12
Lior Vaisman <lior@argon.io>
2022-04-17 17:24:13 +0300

    change test filenames

--- secret/scanner_test.go
+++ secret/scanner_test.go
@@ -64,19 +64,19 @@ func TestSecretScanner(t *testing.T) {
 			},
 		},
 		{
-			name:          "exclude when no keyword found",
-			configPath:    "testdata/config-bad-keywords.yaml",
-			inputFilePath: "testdata/secret.txt",
-			want:          types.Secret{},
-		},
-		{
 			name:          "include when keyword found",
-			configPath:    "testdata/config-want-keywords.yaml",
+			configPath:    "testdata/config-happy-keywords.yaml",
 			inputFilePath: "testdata/secret.txt",
 			want: types.Secret{
 				FilePath: "testdata/secret.txt",
 				Findings: []types.SecretFinding{wantFinding1, wantFinding2},
 			},
+		},
+		{
+			name:          "exclude when no keyword found",
+			configPath:    "testdata/config-sad-keywords.yaml",
+			inputFilePath: "testdata/secret.txt",
+			want:          types.Secret{},
 		},
 		{
 			name:          "allow-rule path",
