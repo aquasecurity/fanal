fa78ad81da03d8b762f2a28555e6f8897d3c4eb6
DmitriyLewen <dmitriy.lewen@smartforce.io>
2022-06-09 10:31:15 +0600

    refactor

--- analyzer/pkg/rpm/marinerdistroless.go
+++ analyzer/pkg/rpm/marinerdistroless.go
@@ -8,13 +8,13 @@ import (
 
 	"github.com/aquasecurity/fanal/analyzer"
 	"github.com/aquasecurity/fanal/types"
-	"github.com/aquasecurity/fanal/utils"
 	"github.com/aquasecurity/go-dep-parser/pkg/io"
+	"golang.org/x/exp/slices"
 	"golang.org/x/xerrors"
 )
 
 func init() {
-	analyzer.RegisterAnalyzer(&marinerDistrolessPkgAnalyzer{})
+	analyzer.RegisterAnalyzer(&rpmqaPkgAnalyzer{})
 }
 
 const versionMarinerDistroless = 1
@@ -24,11 +24,11 @@ var (
 	requiredMarinerDistrolessFiles = []string{"var/lib/rpmmanifest/container-manifest-2"}
 )
 
-// rpmqaPkgAnalyzer parses the output of 
+// rpmqaPkgAnalyzer parses the output of
 // "rpm -qa --qf %{NAME}\t%{VERSION}-%{RELEASE}\t%{INSTALLTIME}\t%{BUILDTIME}\t%{VENDOR}\t(none)\t%{SIZE}\t%{ARCH}\t%{EPOCHNUM}\t%{SOURCERPM}".
 type rpmqaPkgAnalyzer struct{}
 
-func (a marinerDistrolessPkgAnalyzer) Analyze(_ context.Context, input analyzer.AnalysisInput) (*analyzer.AnalysisResult, error) {
+func (a rpmqaPkgAnalyzer) Analyze(_ context.Context, input analyzer.AnalysisInput) (*analyzer.AnalysisResult, error) {
 	pkgs, err := a.parseMarinerDistrolessManifest(input.Content)
 	if err != nil {
 		return nil, xerrors.Errorf("failed to parse mariner distroless 'container-manifest-2' file: %w", err)
@@ -43,28 +43,27 @@ func (a marinerDistrolessPkgAnalyzer) Analyze(_ context.Context, input analyzer.
 	}, nil
 }
 
-func (a marinerDistrolessPkgAnalyzer) parseMarinerDistrolessManifest(r io.ReadSeekerAt) ([]types.Package, error) {
+func (a rpmqaPkgAnalyzer) parseMarinerDistrolessManifest(r io.ReadSeekerAt) ([]types.Package, error) {
 	var pkgs []types.Package
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := scanner.Text()
 		var name, ver, rel, sourceRpm, arch string
 		// %{NAME}\t%{VERSION}-%{RELEASE}\t%{INSTALLTIME}\t%{BUILDTIME}\t%{VENDOR}\t(none)\t%{SIZE}\t%{ARCH}\t%{EPOCHNUM}\t%{SOURCERPM}
-		if s := strings.Fields(line); len(s) != 10 {
-			return nil, xerrors.Errorf("failed to parse a line (%s)", line)		
-			name = s[0]
-			arch = s[7]
-			sourceRpm = s[9]
-			if verRel := strings.Split(s[1], "-"); len(verRel) == 2 {
-				ver = verRel[0]
-				rel = verRel[1]
-			} else {
-				return nil, xerrors.Errorf("failed to split line (%s) : line doesn't have number of version/release")
-			}
-		} else {
-			return nil, xerrors.Errorf("failed to split line (%s) : wrong number of variables", line)
+		s := strings.Split(line, "\t")
+		if len(s) != 10 {
+			return nil, xerrors.Errorf("failed to parse a line (%s)", line)
 		}
-		srcName, srcVer, srcRel, err := splitSourceRpm(sourceRpm)
+		name = s[0]
+		arch = s[7]
+		sourceRpm = s[9]
+		if verRel := strings.Split(s[1], "-"); len(verRel) == 2 {
+			ver = verRel[0]
+			rel = verRel[1]
+		} else {
+			return nil, xerrors.Errorf("failed to split line (%s) : line doesn't have number of version/release")
+		}
+		srcName, srcVer, srcRel, err := splitFileName(sourceRpm)
 		if err != nil {
 			return nil, xerrors.Errorf("failed to split source rpm: %w", err)
 		}
@@ -82,37 +81,14 @@ func (a marinerDistrolessPkgAnalyzer) parseMarinerDistrolessManifest(r io.ReadSe
 	return pkgs, nil
 }
 
-// format: %{NAME}-%{VERSION}-%{RELEASE}.src.rpm
-func splitSourceRpm(sourseRpm string) (name, ver, rel string, err error) {
-	if !strings.HasSuffix(sourseRpm, ".src.rpm") {
-		return "", "", "", xerrors.Errorf("sourceRPM (%s) doesn't contain '.src.rpm' suffix", sourseRpm)
-	}
-	src := sourseRpm[:len(sourseRpm)-8]
-
-	relIndex := strings.LastIndex(src, "-")
-	if relIndex == -1 {
-		return "", "", "", xerrors.Errorf("sourceRPM (%s) doesn't contain release", sourseRpm)
-	}
-	rel = src[relIndex+1:]
-
-	verIndex := strings.LastIndex(src[:relIndex], "-")
-	if verIndex == -1 {
-		return "", "", "", xerrors.Errorf("sourceRPM (%s) doesn't contain version", sourseRpm)
-	}
-	ver = src[verIndex+1 : relIndex]
-
-	name = src[:verIndex]
-	return name, ver, rel, nil
-}
-
-func (a marinerDistrolessPkgAnalyzer) Required(filePath string, _ os.FileInfo) bool {
+func (a rpmqaPkgAnalyzer) Required(filePath string, _ os.FileInfo) bool {
 	return slices.Contains(requiredMarinerDistrolessFiles, filePath)
 }
 
-func (a marinerDistrolessPkgAnalyzer) Type() analyzer.Type {
-	return analyzer.TypeMarinerDistroless
+func (a rpmqaPkgAnalyzer) Type() analyzer.Type {
+	return analyzer.TypeRpmqa
 }
 
-func (a marinerDistrolessPkgAnalyzer) Version() int {
+func (a rpmqaPkgAnalyzer) Version() int {
 	return versionMarinerDistroless
 }
--- analyzer/pkg/rpm/marinerdistroless_test.go
+++ analyzer/pkg/rpm/marinerdistroless_test.go
@@ -53,13 +53,13 @@ glibc	2.35-2.cm2	1653816591	1653628955	Microsoft Corporation	(none)	10855265	x86
 		{
 			name:    "sab path",
 			content: "filesystem\t1.1-7.cm1\t1653164283\t1599428094",
-			wantErr: "failed to split line (filesystem\t1.1-7.cm1\t1653164283\t1599428094) : wrong number of variables",
+			wantErr: "failed to parse a line (filesystem\t1.1-7.cm1\t1653164283\t1599428094)",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			a := marinerDistrolessPkgAnalyzer{}
+			a := rpmqaPkgAnalyzer{}
 			result, err := a.parseMarinerDistrolessManifest(strings.NewReader(test.content))
 			if test.wantErr != "" {
 				assert.NotNil(t, err)
@@ -67,55 +67,6 @@ glibc	2.35-2.cm2	1653816591	1653628955	Microsoft Corporation	(none)	10855265	x86
 			} else {
 				assert.Nil(t, err)
 				assert.Equal(t, test.wantPkgs, result)
-			}
-		})
-	}
-}
-
-func TestSplitSourceRpm(t *testing.T) {
-	tests := []struct {
-		name        string
-		filepath    string
-		wantName    string
-		wantVersion string
-		wantRelease string
-		wantErr     string
-	}{
-		{
-			name:        "happy path",
-			filepath:    "distroless-packages-0.1-2.cm2.src.rpm",
-			wantName:    "distroless-packages",
-			wantVersion: "0.1",
-			wantRelease: "2.cm2",
-		},
-		{
-			name:     "sad path. Bad suffix",
-			filepath: "distroless-packages-0.1-2.cm2.src",
-			wantErr:  "sourceRPM (distroless-packages-0.1-2.cm2.src) doesn't contain '.src.rpm' suffix",
-		},
-		{
-			name:     "sad path. No release",
-			filepath: "filesystem 1.1 9.cm2.src.rpm",
-			wantErr:  "sourceRPM (filesystem 1.1 9.cm2.src.rpm) doesn't contain release",
-		},
-		{
-			name:     "sad path. No version",
-			filepath: "filesystem 1.1-9.cm2.src.rpm",
-			wantErr:  "sourceRPM (filesystem 1.1-9.cm2.src.rpm) doesn't contain version",
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			name, ver, rel, err := splitSourceRpm(test.filepath)
-			if test.wantErr != "" {
-				assert.NotNil(t, err)
-				assert.Equal(t, test.wantErr, err.Error())
-			} else {
-				assert.Nil(t, err)
-				assert.Equal(t, test.wantName, name)
-				assert.Equal(t, test.wantRelease, rel)
-				assert.Equal(t, test.wantVersion, ver)
 			}
 		})
 	}
