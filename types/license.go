c60b03dc279968f1256a1b74ecaea4888cd413f5
Owen Rumney <owen.rumney@aquasec.com>
2022-06-13 13:16:21 +0100

    fix: make sure classification index survives cache
    Signed-off-by: Owen Rumney <owen.rumney@aquasec.com>

--- types/license.go
+++ types/license.go
@@ -12,7 +12,7 @@ type LicenseFinding struct {
 	Confidence                       float64 `json:"match_confidence"`
 	StartLine                        int     `json:"start_line"`
 	EndLine                          int     `json:"end_line"`
-	GoogleLicenseClassificationIndex int     `json:"-"`
+	GoogleLicenseClassificationIndex int     `json:"classification_index"`
 	GoogleLicenseClassification      string  `json:"google_license_classification"`
 	Package                          string  `json:"package,omitempty"`
 	LicenseLink                      string  `json:"license_link,omitempty"`
